107a2f50beb85fbbfc2f65d77dce50bdca35f25d
Stefan Nguyen <stnguyenvn@gmail.com>
2022-10-29 21:47:23 -0400

    Can move around

--- src/editor.rs
+++ src/editor.rs
@@ -1,5 +1,5 @@
 use crossterm::{cursor, event, execute, terminal};
-use event::{Event, KeyCode, KeyModifiers};
+use event::{Event, KeyCode, KeyEvent, KeyModifiers};
 use std::io;
 
 pub struct Editor {
@@ -13,17 +13,18 @@ impl Editor {
 
         // Infinite loop
         loop {
-            self.draw_rows();
+            self.refresh_screen()?;
 
             // event::read() is blocking until an Event is triggered
             match event::read()? {
-                Event::Key(event) => {
-                    if event.modifiers.contains(KeyModifiers::CONTROL)
-                        && event.code == KeyCode::Char('q')
+                Event::Key(key_event) => {
+                    if key_event.modifiers.contains(KeyModifiers::CONTROL)
+                        && key_event.code == KeyCode::Char('q')
                     {
                         break;
                     }
-                    println!("Key event {:?}", event);
+
+                    self.process_key(key_event)?;
                 }
                 _ => (),
             }
@@ -33,21 +34,45 @@ impl Editor {
     }
 
     fn start(&mut self) -> Result<(), io::Error> {
-        let mut stdout = io::stdout();
         execute!(
-            stdout,
+            io::stdout(),
             terminal::EnterAlternateScreen,
-            cursor::EnableBlinking
+            cursor::MoveTo(0, 0),
         )?;
         terminal::enable_raw_mode()?;
         Ok(())
     }
 
     fn stop(&mut self) -> Result<(), io::Error> {
-        let mut stdout = io::stdout();
         terminal::disable_raw_mode()?;
-        execute!(stdout, terminal::LeaveAlternateScreen)?;
+        execute!(io::stdout(), terminal::LeaveAlternateScreen)?;
         println!("Bye!");
+        Ok(())
+    }
+
+    fn process_key(&self, key_event: KeyEvent) -> Result<(), io::Error> {
+        // print!("{:?}", key_event);
+        match key_event.code {
+            KeyCode::Up => execute!(io::stdout(), cursor::MoveUp(1)),
+            KeyCode::Down => execute!(io::stdout(), cursor::MoveDown(1)),
+            KeyCode::Left => execute!(io::stdout(), cursor::MoveLeft(1)),
+            KeyCode::Right => execute!(io::stdout(), cursor::MoveRight(1)),
+
+            // Do nothing
+            _ => Ok(()),
+        }
+    }
+
+    fn refresh_screen(&self) -> Result<(), io::Error> {
+        let mut stdout = io::stdout();
+        execute!(
+            stdout,
+            cursor::SavePosition,
+            terminal::Clear(terminal::ClearType::All),
+            cursor::MoveTo(0, 0),
+        )?;
+        self.draw_rows();
+        execute!(stdout, cursor::RestorePosition,)?;
         Ok(())
     }
 
